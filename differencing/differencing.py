42e2207fef610e289df1c8e837e41cd538258798
HUST-8xbet <dongchayleminhfifa@gmail.com>
2026-02-27 08:55:56 +0700

    Update solution for differencing

--- differencing/differencing.py
+++ differencing/differencing.py
@@ -6,5 +6,5 @@ def differencing(series, order):
     for _ in range(order):
         for i in range(len(series) - 1):
             series[i] = series[i + 1] - series[i]
-        series = series[:len(series) - 1]
+        series = series[:-1]
     return series
